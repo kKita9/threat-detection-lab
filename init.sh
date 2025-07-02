ecfdecda8acd1cb49c5c6a3855b309db484f0506
Karol Kita <kkita970@gmail.com>
2025-07-02 21:54:52 +0200

    update init.sh and docker-compose.yml after adding ftp-lab

--- init.sh
+++ init.sh
@@ -5,13 +5,16 @@ echo "Initializing log directories and files..."
 
 mkdir -p ./logs/ssh-lab
 
-# Tworzymy pliki logów, które będą używane przez rsyslog
+# files used by rsyslog rsyslog
 touch ./logs/ssh-lab/auth.log
 touch ./logs/ssh-lab/syslog
 touch ./logs/ssh-lab/secure
 touch ./logs/ssh-lab/messages
 
-# Ustawiamy odpowiednie uprawnienia
+# ftp logs
+mkdir -p logs/ftp-lab
+touch logs/ftp-lab/vsftpd.log
+
 chmod 666 ./logs/ssh-lab/*.log
 
 echo "Done. You can now run: docker compose up --build -d"
